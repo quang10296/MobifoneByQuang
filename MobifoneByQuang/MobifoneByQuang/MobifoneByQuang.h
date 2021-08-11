30ebf32cde7e96620378d9680a4e411402bbd714
quang10296 <duyquangnguyen10296@gmail.com>
2021-08-11 11:30:34 +0700

    add framework

---
+++ MobifoneByQuang/MobifoneByQuang/MobifoneByQuang.h
@@ -0,0 +1,18 @@
+//
+//  MobifoneByQuang.h
+//  MobifoneByQuang
+//
+//  Created by Quang Nguyễn  on 8/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobifoneByQuang.
+FOUNDATION_EXPORT double MobifoneByQuangVersionNumber;
+
+//! Project version string for MobifoneByQuang.
+FOUNDATION_EXPORT const unsigned char MobifoneByQuangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobifoneByQuang/PublicHeader.h>
+
+
